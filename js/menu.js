8bc2b6efbaf215411542ec4b1de60b416274ee27
gabrielgbraga <ggoesbraga@gmail.com>
2025-07-07 21:02:18 -0300

    delete invited

--- js/menu.js
+++ js/menu.js
@@ -103,8 +103,6 @@ const NavMenu = () => {
         <li style={styles.navItem}><a href="/#contact" style={styles.navLink}>CONTACT</a></li>
         <li style={styles.navSeparator}>|</li>
         <li style={styles.navItem}><a href="../committe.html" style={styles.navLink}>COMMITTEE</a></li>
-        <li style={styles.navSeparator}>|</li>
-        <li style={styles.navItem}><a href="../panelists.html" style={styles.navLink}>PANELISTS</a></li>
       </ul>
     </nav>
   );
